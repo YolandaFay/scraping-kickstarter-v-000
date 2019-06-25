dc8d2695e32e79edfb6e927befa67f3cfc05aab3
Yolanda Fay <yolandafay@gmail.com>
2019-06-25 03:44:16 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,7 +15,15 @@ def create_project_hash
   # location: project.css("ul.project-meta span.location-name").text 
   # percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
   
-  
+  projects = {}
+ 
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+ 
+  # return the projects hash
+  projects
   
 end
 
