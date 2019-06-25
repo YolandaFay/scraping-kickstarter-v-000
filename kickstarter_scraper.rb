2ff14b83942356f95c7d4b522869034418cff17b
Yolanda Fay <yolandafay@gmail.com>
2019-06-25 03:47:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -19,8 +19,8 @@ def create_project_hash
  
   ## project iteration
   kickstarter.css("li.project.grid_4").each do |project|
-    title = 
-    projects[title.to_sym] = {
+    #title = 
+    projects = {
       :title => project.css("h2.bbcard_name strong a").text 
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
